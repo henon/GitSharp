91e8085564ef67a14e2ef2201a851753f6655ea1
Miguel De Icaza <miguel@novell.com>
2008-11-29 17:08:25 -0800

    Used number instead of PlatformID.MacOSX for backward-compatibility

--- Lib/RepositoryConfig.cs
+++ Lib/RepositoryConfig.cs
@@ -99,7 +99,7 @@ namespace Gitty.Lib
 	    string bd;
 
 	    int p = (int) Environment.OSVersion.Platform;
-	    if (p == (int) PlatformID.Unix || p == (int) PlatformID.MacOSX || p == 128)
+	    if (p == (int) PlatformID.Unix || p == 6 /* MacOSX */ || p == 128)
 		bd = Environment.GetFolderPath (Environment.SpecialFolder.Personal);
 	    else
 		bd = Environment.GetEnvironmentVariable( "USERPROFILE");

185f8e1a38f13c3be363e7a77fdf9b8686eb5a6e
Kevin Thompson <mrunleaded@gmail.com>
2008-11-30 22:24:40 -0800

    Completed PackedObjectInfo as it is a dependency of the PackIndexWriter

---
+++ Transport/PackedObjectInfo.cs
@@ -0,0 +1,77 @@
+﻿/*
+ * Copyright (C) 2008, Robin Rosenberg <robin.rosenberg@dewire.com>
+ * Copyright (C) 2008, Shawn O. Pearce <spearce@spearce.org>
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Git Development Community nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gitty.Lib.CSharp.Transport
+{
+    /**
+     * Description of an object stored in a pack file, including offset.
+     * <p>
+     * When objects are stored in packs Git needs the ObjectId and the offset
+     * (starting position of the object data) to perform random-access reads of
+     * objects from the pack. This extension of ObjectId includes the offset.
+     */
+    public class PackedObjectInfo : ObjectId
+    {
+
+        public PackedObjectInfo(long headerOffset, int packedCRC, AnyObjectId id)
+            : base(id)
+        {
+            Offset = headerOffset;
+            CRC = packedCRC;
+        }
+
+        /**
+         * Create a new structure to remember information about an object.
+         *
+         * @param id
+         *            the identity of the object the new instance tracks.
+         */
+        public PackedObjectInfo(AnyObjectId id)
+            : base(id)
+        {
+        }
+
+        public long Offset { get; set; }
+        public int CRC { get; set; }
+    }
+}

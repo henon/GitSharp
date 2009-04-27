66753c708ca0357106ed9af1d698d8b2e0ab54d2
Kevin Thompson <kthompson@pelco.com>
2009-04-27 16:13:47 -0700

    * Exceptions/EntryExistsException.cs: Added new exception

---
+++ Exceptions/EntryExistsException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gitty.Core.Exceptions
+{
+    public class EntryExistsException : Exception
+    {
+        public EntryExistsException(string name)
+            : base(string.Format("Tree entry \"{0}\" already exists.", name))
+        {
+        }
+
+    }
+}

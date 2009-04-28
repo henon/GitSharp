eb23b183fae6f7838fd677c20820cb3fa656aa84
Kevin Thompson <mrunleaded@gmail.com>
2009-04-27 19:58:34 -0700

    * RefUpdate.cs: Implemented several methods

--- RefUpdate.cs
+++ RefUpdate.cs
@@ -46,36 +46,207 @@ namespace Gitty.Core
 {
     public class RefUpdate
     {
-        public enum Result
+        public enum RefUpdateResult
         {
+			/// <summary>
+			/// The ref update/delete has not been attempted by the caller.
+			/// </summary>
             NotAttempted,
+			/// <summary>
+			/// The ref could not be locked for update/delete.
+			/// This is generally a transient failure and is usually caused by
+			/// another process trying to access the ref at the same time as this
+			/// process was trying to update it. It is possible a future operation
+			/// will be successful.
+			/// </summary>
             LockFailure,
+			/// <summary>
+			/// Same value already stored.
+			/// 
+			/// Both the old value and the new value are identical. No change was
+			/// necessary for an update. For delete the branch is removed.
+			/// </summary>
             NoChange,
+			/// <summary>
+			/// The ref was created locally for an update, but ignored for delete.
+			/// <p>
+			/// The ref did not exist when the update started, but it was created
+			/// successfully with the new value.
+			/// </summary>
             New,
+			/// <summary>
+			/// The ref had to be forcefully updated/deleted.
+			/// <p>
+			/// The ref already existed but its old value was not fully merged into
+			/// the new value. The configuration permitted a forced update to take
+			/// place, so ref now contains the new value. History associated with the
+			/// objects not merged may no longer be reachable.
+			/// </summary>
             Forced, 
+			/// <summary>
+			/// The ref was updated/deleted in a fast-forward way.
+			/// <p>
+			/// The tracking ref already existed and its old value was fully merged
+			/// into the new value. No history was made unreachable.
+			/// </summary>
             FastForward,
+			/// <summary>
+			/// Not a fast-forward and not stored.
+			/// <p>
+			/// The tracking ref already existed but its old value was not fully
+			/// merged into the new value. The configuration did not allow a forced
+			/// update/delete to take place, so ref still contains the old value. No
+			/// previous history was lost.
+			/// </summary>
             Rejected,
+			/// <summary>
+			/// Rejected because trying to delete the current branch.
+			/// <p>
+			/// Has no meaning for update.
+			/// </summary>
+			RejectedCurrentBranch,
+			/// <summary>
+			/// The ref was probably not updated/deleted because of I/O error.
+			/// <p>
+			/// Unexpected I/O error occurred when writing new ref. Such error may
+			/// result in uncertain state, but most probably ref was not updated.
+			/// <p>
+			/// This kind of error doesn't include {@link #LOCK_FAILURE}, which is a
+			/// different case.
+			/// </summary>
             IOFailure,
         }
 
-        public RefUpdate(RefDatabase r, Ref refObject, FileInfo f)
-        {
+		/** Repository the ref is stored in. */
+		private RefDatabase db;
+	
+		/** Location of the loose file holding the value of this ref. */
+		private FileInfo looseFile;
+	
+		/** New value the caller wants this ref to have. */
+		private ObjectId newValue;
+	
+		/** Message the caller wants included in the reflog. */
+		private String refLogMessage;
+	
+		/** Should the Result value be appended to {@link #refLogMessage}. */
+		private bool refLogIncludeResult;
+	
+		/** If non-null, the value {@link #oldValue} must have to continue. */
+		private ObjectId expValue;
+	
+		private Ref _ref;
+		
+ 		public RefUpdate(RefDatabase refDb, Ref r, FileInfo f)
+		{
+			db = refDb;
+			this._ref = r;
+			this.OldObjectId = r.ObjectId;
+			looseFile = f;
+			this.Result = RefUpdateResult.NotAttempted;
+		}
+		
+		public Repository Repository
+		{
+			get
+			{
+				return this.db.Repository;
+			}
+		}
+		
+		public string Name
+		{
+			get
+			{
+				return _ref.Name;
+			}
+		}
 
+        public ObjectId NewObjectId
+        {
+			get
+			{
+				return newValue;
+			}
+			set
+			{
+				newValue = value.Copy();
+			}
         }
 
-        internal void SetNewObjectId(ObjectId id)
+		public ObjectId ExpectedOldObjectId
+		{
+			get
+			{
+				return expValue;
+			}
+			set
+			{
+				expValue = value != null ? value.ToObjectId() : null;
+			}
+		}
+		
+		public bool IsForceUpdate {get; set;}
+		
+		public PersonIdent RefLogIdent { get; set; }
+		
+		public ObjectId OldObjectId { get; private set; }
+		
+		public RefUpdateResult Result { get; private set; }
+		
+		
+		public string GetRefLogMessage()
+		{
+			return refLogMessage;
+		}
+		
+        public void SetRefLogMessage(string msg, bool appendStatus)
         {
-            throw new NotImplementedException();
+            refLogMessage = msg;
+			refLogIncludeResult = appendStatus;
         }
+		
+		private void RequireCanDoUpdate() {
+			if (newValue == null)
+				throw new InvalidOperationException("A NewObjectId is required.");
+		}
 
-        internal void SetRefLogMessage(string p, bool p_2)
-        {
-            throw new NotImplementedException();
-        }
 
-        internal Result ForceUpdate()
+        public RefUpdateResult ForceUpdate()
         {
-            throw new NotImplementedException();
+            this.IsForceUpdate = true;
+			return Update();
         }
+		
+		public RefUpdateResult Update() 
+		{
+			throw new NotImplementedException();	
+		}
+		
+		public RefUpdateResult Delete() 
+		{
+			throw new NotImplementedException();	
+		}		
+		
+		private abstract class StoreBase {
+			public abstract RefUpdateResult Store(LockFile lockFile, RefUpdateResult status);
+		}
+		
+		private class UpdateStore : StoreBase
+		{
+				public override RefUpdateResult Store (LockFile lockFile, RefUpdateResult status)
+				{
+					throw new NotImplementedException();
+				}			
+		}
+		
+		private static int Count(string s, char c)
+		{
+			int count = 0;
+			for (int p = s.IndexOf(c); p >= 0; p = s.IndexOf(c, p + 1)) {
+				count++;
+			}
+			return count;
+		}
     }
 }

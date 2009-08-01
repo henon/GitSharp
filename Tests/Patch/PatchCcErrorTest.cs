1698199ecb105019fe238c545e9979a8f0c46165
Gil Ran <gilrun@gmail.com>
2009-08-02 00:59:01 +0300

    Fixing an index out of bounds bug in CombinedFileHeader.cs

---
+++ Tests/Patch/PatchCcErrorTest.cs
@@ -0,0 +1,109 @@
+﻿/*
+ * Copyright (C) 2008, Google Inc.
+ * Copyright (C) 2009, Gil Ran <gilrun@gmail.com>
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Git Development Community nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using GitSharp.Diff;
+using GitSharp.Patch;
+using NUnit.Framework;
+using System.IO;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace GitSharp.Tests.Patch
+{
+    [TestFixture]
+    public class PatchCcErrorTest
+    {
+        private readonly string PATCHS_DIR = "../../../Tests/Patch/Resources/";
+
+        [Test]
+	    public void testError_CcTruncatedOld()
+        {
+		    GitSharp.Patch.Patch p = parseTestPatchFile();
+		    Assert.AreEqual(1, p.getFiles().Count);
+		    Assert.AreEqual(3, p.getErrors().Count);
+		    {
+			    FormatError e = p.getErrors()[0];
+			    Assert.AreSame(FormatError.Severity.ERROR, e.getSeverity());
+			    Assert.AreEqual(
+					    "Truncated hunk, at least 1 lines is missing for ancestor 1",
+					    e.getMessage());
+			    Assert.AreEqual(346, e.getOffset());
+			    Assert.IsTrue(e.getLineText().StartsWith(
+					    "@@@ -55,12 -163,13 +163,15 @@@ public "));
+		    }
+		    {
+			    FormatError e = p.getErrors()[1];
+			    Assert.AreSame(FormatError.Severity.ERROR, e.getSeverity());
+			    Assert.AreEqual(
+					    "Truncated hunk, at least 2 lines is missing for ancestor 2",
+					    e.getMessage());
+			    Assert.AreEqual(346, e.getOffset());
+			    Assert.IsTrue(e.getLineText().StartsWith(
+					    "@@@ -55,12 -163,13 +163,15 @@@ public "));
+		    }
+		    {
+			    FormatError e = p.getErrors()[2];
+			    Assert.AreSame(FormatError.Severity.ERROR, e.getSeverity());
+			    Assert.AreEqual("Truncated hunk, at least 3 new lines is missing", e
+					    .getMessage());
+			    Assert.AreEqual(346, e.getOffset());
+			    Assert.IsTrue(e.getLineText().StartsWith(
+					    "@@@ -55,12 -163,13 +163,15 @@@ public "));
+		    }
+	    }
+
+        private GitSharp.Patch.Patch parseTestPatchFile()
+        {
+            string patchFile = (new StackFrame(1, true)).GetMethod().Name + ".patch";
+            Stream inStream = new FileStream(PATCHS_DIR + patchFile, System.IO.FileMode.Open);
+		    if (inStream == null)
+            {
+			    Assert.Fail("No " + patchFile + " test vector");
+			    return null; // Never happens
+		    }
+		    try {
+			    GitSharp.Patch.Patch p = new GitSharp.Patch.Patch();
+			    p.parse(inStream);
+			    return p;
+		    } finally {
+			    inStream.Close();
+		    }
+	    }
+
+    }
+}

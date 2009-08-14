076245035d539d1435b91f4fd846158d0cf0e7b4
Adriano Machado <adriano.m.machado@hotmail.com>
2009-08-14 17:47:29 -0300

    Converted the PatchTest.cs tests

---
+++ Tests/Patch/BasePatchTest.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using System.Text;
+using GitSharp.Patch;
+using NUnit.Framework;
+
+namespace GitSharp.Tests.Patch
+{
+	public class BasePatchTest
+	{
+		protected const string PATCHS_DIR = "../../../Tests/Patch/Resources/";
+
+		protected GitSharp.Patch.Patch parseTestPatchFile(string patchFile)
+		{
+			try
+			{
+				using (Stream inStream = new FileStream(patchFile, System.IO.FileMode.Open))
+				{
+					GitSharp.Patch.Patch p = new GitSharp.Patch.Patch();
+					p.parse(inStream);
+					return p;
+				}
+			}
+			catch(IOException)
+			{
+				Assert.Fail("No " + patchFile + " test vector");
+				return null; // Never happens
+			}
+		}
+
+		protected string GetAllErrorsFromPatch(GitSharp.Patch.Patch patch)
+		{
+			if (patch == null || patch.getErrors().Count == 0)
+			{
+				return string.Empty;
+			}
+
+			StringBuilder sb = new StringBuilder();
+
+			foreach (FormatError formatError in patch.getErrors())
+			{
+				sb.AppendLine(formatError.getMessage());
+			}
+
+			return sb.ToString();
+		}
+	}
+}

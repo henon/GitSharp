b60313a3d96dd8351330054fc7c10c720c5e0f9a
caytchen <caytchen@gmail.com>
2009-08-25 21:02:25 +0200

    Ported PacketLineInTests. 21 Tests, 9 fail.
    Signed-off-by: caytchen <caytchen@gmail.com>

--- GitSharp/Transport/PacketLineIn.cs
+++ GitSharp/Transport/PacketLineIn.cs
@@ -58,11 +58,11 @@ namespace GitSharp.Transport
 
             for (char i = '0'; i <= '9'; i++)
             {
-                    fromhex[i] = (byte)(i - '0');
+                    ret[i] = (byte)(i - '0');
             }
             for (char i = 'a'; i <= 'f'; i++)
             {
-                fromhex[i] = (byte) ((i - 'a') + 10);
+                ret[i] = (byte) ((i - 'a') + 10);
             }
 
             return ret;

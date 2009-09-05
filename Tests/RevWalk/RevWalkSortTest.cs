a86c9b8933b117d9ba029015d70748cd3b2d4c2e
nulltoken <emeric.fermas@gmail.com>
2009-09-05 20:41:27 +0200

    RevWalkSortTest converted to C#. Tests fail.

--- Tests/RevWalk/RevWalkSortTest.cs
+++ Tests/RevWalk/RevWalkSortTest.cs
@@ -37,33 +37,34 @@
 
 using GitSharp.Tests.Util;
 using GitSharp.RevWalk;
+using NUnit.Framework;
+
 namespace GitSharp.Tests.RevWalk
 {
-
-    using NUnit.Framework;
     [TestFixture]
     public class RevWalkSortTest : RevWalkTestCase
     {
-#if false
-	public void testSort_Default() throws Exception {
-		final RevCommit a = commit();
-		final RevCommit b = commit(1, a);
-		final RevCommit c = commit(1, b);
-		final RevCommit d = commit(1, c);
+    [Test]
+    public void testSort_Default() {
+		RevCommit a = commit();
+		RevCommit b = commit(1, a);
+		RevCommit c = commit(1, b);
+		RevCommit d = commit(1, c);
 
 		markStart(d);
 		assertCommit(d, rw.next());
 		assertCommit(c, rw.next());
 		assertCommit(b, rw.next());
 		assertCommit(a, rw.next());
-		assertNull(rw.next());
+		Assert.IsNull(rw.next());
 	}
 
-	public void testSort_COMMIT_TIME_DESC() throws Exception {
-		final RevCommit a = commit();
-		final RevCommit b = commit(a);
-		final RevCommit c = commit(b);
-		final RevCommit d = commit(c);
+    [Test]
+    public void testSort_COMMIT_TIME_DESC() {
+		RevCommit a = commit();
+		RevCommit b = commit(a);
+		RevCommit c = commit(b);
+		RevCommit d = commit(c);
 
 		rw.sort(RevSort.COMMIT_TIME_DESC);
 		markStart(d);
@@ -71,14 +72,15 @@ namespace GitSharp.Tests.RevWalk
 		assertCommit(c, rw.next());
 		assertCommit(b, rw.next());
 		assertCommit(a, rw.next());
-		assertNull(rw.next());
+		Assert.IsNull(rw.next());
 	}
 
-	public void testSort_REVERSE() throws Exception {
-		final RevCommit a = commit();
-		final RevCommit b = commit(a);
-		final RevCommit c = commit(b);
-		final RevCommit d = commit(c);
+    [Test]
+    public void testSort_REVERSE() {
+		RevCommit a = commit();
+		RevCommit b = commit(a);
+		RevCommit c = commit(b);
+		RevCommit d = commit(c);
 
 		rw.sort(RevSort.REVERSE);
 		markStart(d);
@@ -86,19 +88,20 @@ namespace GitSharp.Tests.RevWalk
 		assertCommit(b, rw.next());
 		assertCommit(c, rw.next());
 		assertCommit(d, rw.next());
-		assertNull(rw.next());
+		Assert.IsNull(rw.next());
 	}
 
-	public void testSort_COMMIT_TIME_DESC_OutOfOrder1() throws Exception {
+    [Test]
+    public void testSort_COMMIT_TIME_DESC_OutOfOrder1() {
 		// Despite being out of order time-wise, a strand-of-pearls must
 		// still maintain topological order.
 		//
-		final RevCommit a = commit();
-		final RevCommit b = commit(a);
-		final RevCommit c = commit(-5, b);
-		final RevCommit d = commit(10, c);
-		assertTrue(parse(a).getCommitTime() < parse(d).getCommitTime());
-		assertTrue(parse(c).getCommitTime() < parse(b).getCommitTime());
+		RevCommit a = commit();
+		RevCommit b = commit(a);
+		RevCommit c = commit(-5, b);
+		RevCommit d = commit(10, c);
+		Assert.IsTrue(parse(a).getCommitTime() < parse(d).getCommitTime());
+		Assert.IsTrue(parse(c).getCommitTime() < parse(b).getCommitTime());
 
 		rw.sort(RevSort.COMMIT_TIME_DESC);
 		markStart(d);
@@ -106,17 +109,18 @@ namespace GitSharp.Tests.RevWalk
 		assertCommit(c, rw.next());
 		assertCommit(b, rw.next());
 		assertCommit(a, rw.next());
-		assertNull(rw.next());
+		Assert.IsNull(rw.next());
 	}
 
-	public void testSort_COMMIT_TIME_DESC_OutOfOrder2() throws Exception {
+    [Test]
+    public void testSort_COMMIT_TIME_DESC_OutOfOrder2() {
 		// c1 is back dated before its parent.
 		//
-		final RevCommit a = commit();
-		final RevCommit b = commit(a);
-		final RevCommit c1 = commit(-5, b);
-		final RevCommit c2 = commit(10, b);
-		final RevCommit d = commit(c1, c2);
+		RevCommit a = commit();
+		RevCommit b = commit(a);
+		RevCommit c1 = commit(-5, b);
+		RevCommit c2 = commit(10, b);
+		RevCommit d = commit(c1, c2);
 
 		rw.sort(RevSort.COMMIT_TIME_DESC);
 		markStart(d);
@@ -125,17 +129,18 @@ namespace GitSharp.Tests.RevWalk
 		assertCommit(b, rw.next());
 		assertCommit(a, rw.next());
 		assertCommit(c1, rw.next());
-		assertNull(rw.next());
+		Assert.IsNull(rw.next());
 	}
 
-	public void testSort_TOPO() throws Exception {
+    [Test]
+    public void testSort_TOPO() {
 		// c1 is back dated before its parent.
 		//
-		final RevCommit a = commit();
-		final RevCommit b = commit(a);
-		final RevCommit c1 = commit(-5, b);
-		final RevCommit c2 = commit(10, b);
-		final RevCommit d = commit(c1, c2);
+		RevCommit a = commit();
+		RevCommit b = commit(a);
+		RevCommit c1 = commit(-5, b);
+		RevCommit c2 = commit(10, b);
+		RevCommit d = commit(c1, c2);
 
 		rw.sort(RevSort.TOPO);
 		markStart(d);
@@ -144,17 +149,18 @@ namespace GitSharp.Tests.RevWalk
 		assertCommit(c1, rw.next());
 		assertCommit(b, rw.next());
 		assertCommit(a, rw.next());
-		assertNull(rw.next());
+		Assert.IsNull(rw.next());
 	}
 
-	public void testSort_TOPO_REVERSE() throws Exception {
+    [Test]
+    public void testSort_TOPO_REVERSE() {
 		// c1 is back dated before its parent.
 		//
-		final RevCommit a = commit();
-		final RevCommit b = commit(a);
-		final RevCommit c1 = commit(-5, b);
-		final RevCommit c2 = commit(10, b);
-		final RevCommit d = commit(c1, c2);
+		RevCommit a = commit();
+		RevCommit b = commit(a);
+		RevCommit c1 = commit(-5, b);
+		RevCommit c2 = commit(10, b);
+		RevCommit d = commit(c1, c2);
 
 		rw.sort(RevSort.TOPO);
 		rw.sort(RevSort.REVERSE, true);
@@ -164,8 +170,7 @@ namespace GitSharp.Tests.RevWalk
 		assertCommit(c1, rw.next());
 		assertCommit(c2, rw.next());
 		assertCommit(d, rw.next());
-		assertNull(rw.next());
+		Assert.IsNull(rw.next());
 	}
-#endif
     }
 }

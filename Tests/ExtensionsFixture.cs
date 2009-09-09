50e02097a50d2c9e537165e7fa3ea491216b5029
nulltoken <emeric.fermas@gmail.com>
2009-09-09 13:15:13 +0200

    Added some tests to cover IsFile() and IsDirectory()

---
+++ Tests/ExtensionsFixture.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using NUnit.Framework;
+
+namespace GitSharp.Tests
+{
+    [TestFixture]
+    public class ExtensionsFixture
+    {
+        [Test]
+        public void IsDirectory()
+        {
+            var filePath = Assembly.GetExecutingAssembly().Location;
+            var directoryPath = Environment.CurrentDirectory;
+
+            Assert.IsTrue(new FileInfo(directoryPath).IsDirectory());
+            Assert.IsTrue(new DirectoryInfo(directoryPath).IsDirectory());
+            Assert.IsFalse(new FileInfo(filePath).IsDirectory());
+            Assert.IsFalse(new DirectoryInfo(filePath).IsDirectory());
+        }
+
+        [Test]
+        public void IsFile()
+        {
+            var filePath = Assembly.GetExecutingAssembly().Location;
+            var directoryPath = Environment.CurrentDirectory;
+
+            Assert.IsTrue(new FileInfo(filePath).IsFile());
+            Assert.IsTrue(new DirectoryInfo(filePath).IsFile());
+            Assert.IsFalse(new FileInfo(directoryPath).IsFile());
+            Assert.IsFalse(new DirectoryInfo(directoryPath).IsFile());
+        }
+    }
+}

d6c1e6d3480070caf4637a8f64751f47e401ce39
nulltoken <emeric.fermas@gmail.com>
2009-10-09 21:11:23 +0200

    Narrowing down Mono problem related to DateTimeOffset.

--- Tests/API/CommitDateTests.cs
+++ Tests/API/CommitDateTests.cs
@@ -8,15 +8,26 @@ namespace GitSharp.Tests.API
     public class CommitDateTests : RepositoryTestCase
     {
         private const string KnownCommit = "49322bb17d3acc9146f98c97d078513228bbf3c0";
-        private static readonly DateTimeOffset ExpectedDate = DateTimeOffset.Parse("2005-04-07 15:32:13 -07:00");
+        private static readonly DateTimeOffset _expectedDate = new DateTimeOffset(2005, 04, 07, 15, 32, 13, new TimeSpan(-7, 0, 0));
 
         [Test]
         public void ShouldBeAbleToReadAuthorDate()
         {
+            var expectedDate = DateTimeOffset.Parse("2005-04-07 15:32:13 -07:00");
+
             var repos = new Repository(db);
             var commit = new Commit(repos, KnownCommit);
 
-            Assert.AreEqual(ExpectedDate, commit.AuthorDate);
+            Assert.AreEqual(expectedDate, commit.AuthorDate);
+        }
+
+        [Test]
+        public void ShouldBeAbleToReadAuthorDate2()
+        {
+            var repos = new Repository(db);
+            var commit = new Commit(repos, KnownCommit);
+
+            Assert.AreEqual(_expectedDate, commit.AuthorDate);
         }
 
         [Test]
@@ -25,7 +36,7 @@ namespace GitSharp.Tests.API
             var repos = new Repository(db);
             var commit = new Commit(repos, KnownCommit);
 
-            Assert.AreEqual(ExpectedDate, commit.CommitDate);
+            Assert.AreEqual(_expectedDate, commit.CommitDate);
         }
     }
 }

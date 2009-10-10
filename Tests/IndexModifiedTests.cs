8b40a7a9c8859e20f68a81e645bab6ac14a56973
nulltoken <emeric.fermas@gmail.com>
2009-10-10 22:26:44 +0200

    Added a test to IndexModifiedTests to prove the failure identified by jagregory (cf.  http://groups.google.com/group/gitsharp/msg/fc9dd91fde9c3c03). The test fails.

--- Tests/IndexModifiedTests.cs
+++ Tests/IndexModifiedTests.cs
@@ -22,5 +22,21 @@ namespace GitSharp.Tests
 
             Assert.IsFalse(entry.IsModified(file, true));
         }
+
+        [Test]
+        public void ShouldSupportNotModifiedExtensionlessFilesWithoutContentChecking()
+        {
+            var index = new GitIndex(db);
+
+            writeTrashFile("extensionless-file", "contents");
+
+            var file = new FileInfo(Path.Combine(trash.FullName, "extensionless-file"));
+
+            index.add(trash, file);
+
+            var entry = index.GetEntry("extensionless-file");
+
+            Assert.IsFalse(entry.IsModified(file));
+        }
     }
 }

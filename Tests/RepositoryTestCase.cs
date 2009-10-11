548a3df1ac362db2c6c7d52f8d1cb73648216c7e
nulltoken <emeric.fermas@gmail.com>
2009-10-11 13:55:02 +0200

    While working on GitIndex writing issues, discovered some resource files that haven't been previously added to the project

--- Tests/RepositoryTestCase.cs
+++ Tests/RepositoryTestCase.cs
@@ -241,6 +241,7 @@ namespace GitSharp.Tests
 				"pack-df2982f284bbabb6bdb59ee3fcc6eb0983e20371",
 				"pack-9fb5b411fe6dfa89cc2e6b89d2bd8e5de02b5745",
 				"pack-546ff360fe3488adb20860ce3436a2d6373d2796",
+				"pack-cbdeda40019ae0e6e789088ea0f51f164f489d14",
 				"pack-e6d07037cbcf13376308a0a995d1fa48f8f76aaa",
 				"pack-3280af9c07ee18a87705ef50b0cc4cd20266cf12"
 		    };

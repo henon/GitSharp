e48715f381db9413ec88187d2429d76b3b00cb68
nulltoken <emeric.fermas@gmail.com>
2009-10-21 23:38:50 +0200

    Force the framework the assembly is built against  through Nant dedicated parameter. Made GitSharp.Core compile.

--- package-GitSharp.cmd
+++ package-GitSharp.cmd
@@ -1,2 +1,2 @@
-tools\nant\nant.exe "-buildfile:GitSharp.build" "-D:build.config=release" "-D:build.platform=net-3.5" "-D:build.vcs.number=%BUILD_VCS_NUMBER%" clean dist
+tools\nant\nant.exe "-buildfile:GitSharp.build" "-t:net-3.5" "-D:build.config=release" "-D:build.vcs.number=%BUILD_VCS_NUMBER%" clean dist
 REM pause
--- runtests-GitSharp.cmd
+++ runtests-GitSharp.cmd
@@ -1,2 +1,2 @@
-tools\nant\nant.exe "-buildfile:GitSharp.build" "-D:build.config=debug" "-D:build.vcs.number=%BUILD_VCS_NUMBER%"
+tools\nant\nant.exe "-buildfile:GitSharp.build" "-t:net-3.5" "-D:build.config=debug" "-D:build.vcs.number=%BUILD_VCS_NUMBER%"
 REM pause

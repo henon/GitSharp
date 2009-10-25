c194296a7df27b8750d54829d43919f2597eea24
henon <meinrad.recheis@gmail.com>
2009-10-25 11:11:12 +0100

    reimplemented Index.cs using DirCache

--- GitSharp/Index.cs
+++ GitSharp/Index.cs
@@ -1,4 +1,41 @@
-﻿using System;
+﻿/*
+ * Copyright (C) 2009, Henon <meinrad.recheis@gmail.com>
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Git Development Community nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -43,6 +80,7 @@ namespace Git
         {
             try
             {
+                _index.Lock();
                 var builder = _index.builder();
                 foreach (var path in paths)
                 {
@@ -55,6 +93,7 @@ namespace Git
                 }
                 builder.finish();
                 _index.write();
+                _index.commit(); // save index, does not create a commit object.
             }
             finally
             {
@@ -70,7 +109,7 @@ namespace Git
 
         private void AddDirectory(DirectoryInfo path, DirCacheBuilder builder)
         {
-             AddRecursively(path, builder);
+            AddRecursively(path, builder);
         }
 
         private void AddRecursively(DirectoryInfo dir, DirCacheBuilder builder)
@@ -86,7 +125,16 @@ namespace Git
         /// </summary>
         public void Write()
         {
-            _index.write();
+            try
+            {
+                _index.Lock();
+                _index.write();
+                _index.commit(); // save index, does not create a commit object.
+            }
+            finally
+            {
+                _index.unlock();
+            }
         }
 
         /// <summary>

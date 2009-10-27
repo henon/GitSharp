d13d62b661a42bc40f1fbb326fedc95474f139ba
nulltoken <emeric.fermas@gmail.com>
2009-10-27 22:22:57 +0100

    ﻿As AssemblyVersionAttribute only allows numeric value for each part, commit hash is now part of AssemblyProductAttribute.

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -2,8 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyProduct("GitSharp")]
-
 [assembly: AssemblyCompany("The Git Development Community")]
 [assembly: AssemblyCopyright("Copyright © 2009 The GitSharp Team")]
 [assembly: ComVisible(false)]

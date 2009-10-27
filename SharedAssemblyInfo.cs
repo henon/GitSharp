17817154a0cd8acee4f3a9a1e612ac9cbc483ee1
nulltoken <emeric.fermas@gmail.com>
2009-10-27 21:30:39 +0100

    ﻿Added SharedAssemblyInfo.cs which bears common assembly attributes. Removed from every AssemblyInfo.cs every useless/duplicate information. Version number generation will be handled by the NAnt script.

---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyProduct("GitSharp")]
+
+[assembly: AssemblyCompany("The Git Development Community")]
+[assembly: AssemblyCopyright("Copyright © 2009 The GitSharp Team")]
+[assembly: ComVisible(false)]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif

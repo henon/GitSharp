410a26b5748ce05ac57c5895c95f51d11ba052ae
PauPaw <paulus@3di.jp>
2009-11-04 17:49:33 +0900

    Fixed unittest for Shift_JIS test case.

--- GitSharp.Tests/API/CommitTests.cs
+++ GitSharp.Tests/API/CommitTests.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using NUnit.Framework;
 using System.IO;
+using System.Text;
 
 namespace GitSharp.Tests.API
 {
@@ -37,49 +38,49 @@ namespace GitSharp.Tests.API
             }
         }
 
-        [Test]
-        public void Commit_into_empty_repository_forShiftJist1()
-        {
-            var workingDirectory = Path.Combine(trash.FullName, "test1");
-            using (Repository repo = Repository.Init(workingDirectory))
-            {
-                //GitSharp.Core.Constants.setCHARSET("Shift_JIS");
-                string filepath = Path.Combine(workingDirectory, "for henon.txt");
-                File.WriteAllText(filepath, "Weißbier");
-                repo.Index.Add(filepath);
-                string filepath1 = Path.Combine(workingDirectory, "for nulltoken.txt");
-                File.WriteAllText(filepath1, "Rotwein");
-                repo.Index.Add(filepath1);
-                string filepath2 = Path.Combine(workingDirectory, "俺のためだ.txt");
-                File.WriteAllText(filepath2, "西東京市");
-                repo.Index.Add(filepath2);
-                var commit = repo.Commit("Hello World!日本からShift_JISのためをコミットしました", new Author("ポウルス", "paupaw@tokyo-dome.com"));
-                Assert.NotNull(commit);
-                Assert.IsTrue(commit.IsCommit);
-                Assert.IsNull(commit.Parent);
-                Assert.AreEqual("ポウルス", commit.Author.Name);
-                Assert.AreEqual("paupaw@tokyo-dome.com", commit.Author.EmailAddress);
-                Assert.AreEqual("Hello World!日本からShift_JISのためをコミットしました", commit.Message);
-                // TODO: check if tree contains for henon and for nulltoken, get the blobs and check  the content.
-                Assert.AreEqual(commit, repo.Head.CurrentCommit);
-                var changes = commit.Changes.ToDictionary(change => change.Name);
-                Assert.AreEqual(ChangeType.Added, changes["for henon.txt"].ChangeType);
-                Assert.AreEqual(ChangeType.Added, changes["for nulltoken.txt"].ChangeType);
-                Assert.AreEqual(ChangeType.Added, changes["俺のためだ.txt"].ChangeType);
-                Assert.AreEqual("Weißbier", (changes["for henon.txt"].ComparedObject as Blob).Data);
-                Assert.AreEqual("Rotwein", (changes["for nulltoken.txt"].ComparedObject as Blob).Data);
-                Assert.AreEqual("西東京市", (changes["俺のためだ.txt"].ComparedObject as Blob).Data);
-                Assert.AreEqual(3, changes.Count);
-            }
-        }
+        //[Test]
+        //public void Commit_into_empty_repository_forShiftJis1()
+        //{
+        //    var workingDirectory = Path.Combine(trash.FullName, "test1");
+        //    using (Repository repo = Repository.Init(workingDirectory))
+        //    {
+        //        //GitSharp.Core.Constants.setCHARSET("Shift_JIS");
+        //        string filepath = Path.Combine(workingDirectory, "for henon.txt");
+        //        File.WriteAllText(filepath, "Weißbier");
+        //        repo.Index.Add(filepath);
+        //        string filepath1 = Path.Combine(workingDirectory, "for nulltoken.txt");
+        //        File.WriteAllText(filepath1, "Rotwein");
+        //        repo.Index.Add(filepath1);
+        //        string filepath2 = Path.Combine(workingDirectory, "俺のためだ.txt");
+        //        File.WriteAllText(filepath2, "西東京市");
+        //        repo.Index.Add(filepath2);
+        //        var commit = repo.Commit("Hello World!日本からShift_JISのためをコミットしました", new Author("ポウルス", "paupaw@tokyo-dome.com"));
+        //        Assert.NotNull(commit);
+        //        Assert.IsTrue(commit.IsCommit);
+        //        Assert.IsNull(commit.Parent);
+        //        Assert.AreEqual("ポウルス", commit.Author.Name);
+        //        Assert.AreEqual("paupaw@tokyo-dome.com", commit.Author.EmailAddress);
+        //        Assert.AreEqual("Hello World!日本からShift_JISのためをコミットしました", commit.Message);
+        //        // TODO: check if tree contains for henon and for nulltoken, get the blobs and check  the content.
+        //        Assert.AreEqual(commit, repo.Head.CurrentCommit);
+        //        var changes = commit.Changes.ToDictionary(change => change.Name);
+        //        Assert.AreEqual(ChangeType.Added, changes["for henon.txt"].ChangeType);
+        //        Assert.AreEqual(ChangeType.Added, changes["for nulltoken.txt"].ChangeType);
+        //        Assert.AreEqual(ChangeType.Added, changes["俺のためだ.txt"].ChangeType);
+        //        Assert.AreEqual("Weißbier", (changes["for henon.txt"].ComparedObject as Blob).Data);
+        //        Assert.AreEqual("Rotwein", (changes["for nulltoken.txt"].ComparedObject as Blob).Data);
+        //        Assert.AreEqual("西東京市", (changes["俺のためだ.txt"].ComparedObject as Blob).Data);
+        //        Assert.AreEqual(3, changes.Count);
+        //    }
+        //}
 
         [Test]
-        public void Commit_into_empty_repository_forShiftJist2()
+        public void Commit_into_empty_repository_forShiftJIS()
         {
-            var workingDirectory = Path.Combine(trash.FullName, "test2");
+            var workingDirectory = Path.Combine(trash.FullName, "Shift_JISEncodingTest");
             using (Repository repo = Repository.Init(workingDirectory))
             {
-                //GitSharp.Core.Constants.setCHARSET("Shift_JIS");
+                Encoding shiftJISEncoding = Encoding.GetEncoding("Shift_JIS");
                 
                 string filepath = Path.Combine(workingDirectory, @"Resources\encodingTestData\Shift_JIS\ウサギちゃん\Rabbitはウサギです.txt");
                 System.IO.Directory.CreateDirectory(System.IO.Path.GetDirectoryName(filepath));
@@ -103,11 +104,8 @@ namespace GitSharp.Tests.API
                 var changes = commit.Changes.ToDictionary(change => change.Name);
                 Assert.AreEqual(ChangeType.Added, changes["Rabbitはウサギです.txt"].ChangeType);
                 Assert.AreEqual(ChangeType.Added, changes["新宿三丁目.txt"].ChangeType);
-                Assert.AreEqual(System.Text.Encoding.GetEncoding("Shift_JIS").GetBytes("ラビットis usagi desu."), (changes["Rabbitはウサギです.txt"].ComparedObject as Blob).RawData);
-                Assert.AreEqual(System.Text.Encoding.GetEncoding("Shift_JIS").GetBytes("電車で行きます。"), (changes["新宿三丁目.txt"].ComparedObject as Blob).RawData);
-                Assert.AreNotEqual(System.Text.Encoding.GetEncoding("UTF-8").GetBytes("ラビットis usagi desu."), (changes["Rabbitはウサギです.txt"].ComparedObject as Blob).RawData);
-                Assert.AreNotEqual(System.Text.Encoding.GetEncoding("UTF-8").GetBytes("電車で行きます。"), (changes["新宿三丁目.txt"].ComparedObject as Blob).RawData);
-
+                Assert.AreEqual(Encoding.UTF8.GetBytes("ラビットis usagi desu."), Encoding.Convert(shiftJISEncoding, Encoding.UTF8, (changes["Rabbitはウサギです.txt"].ComparedObject as Blob).RawData)); //Convert from Shift_JIS to UTF-8
+                Assert.AreEqual(Encoding.UTF8.GetBytes("電車で行きます。"), Encoding.Convert(shiftJISEncoding, Encoding.UTF8, (changes["新宿三丁目.txt"].ComparedObject as Blob).RawData)); //Convert from Shift_JIS to UTF-8
                 Assert.AreEqual(2, changes.Count);
             }
         }

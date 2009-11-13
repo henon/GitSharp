4189eacf1789fa84a6d17ba23cccf3d41b8a1d7d
Paulus <paupawsan-learning@yahoo.com>
2009-11-13 09:37:03 +0900

    Adding reset hard test under new nunit Branch test class.

---
+++ GitSharp.Tests/API/BranchTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.IO;
+
+namespace GitSharp.Tests.API
+{
+    [TestFixture]
+    class BranchTest : ApiTestCase
+    {
+        [Test]
+        public void ResetHard()
+        {
+            using (Repository repo = GetTrashRepository())
+            {
+                string filepath = Path.Combine(repo.WorkingDirectory, "a present for paupaw.txt");
+                File.WriteAllText(filepath, "hey, paupaw gets new shoes!");
+                repo.Index.Add(filepath);
+                var commit = repo.Commit("You feeling lucky punk!?", new Author("IronHide", "transformers@cybertron.com"));
+
+                filepath = Path.Combine(repo.WorkingDirectory, "Bintang Kecil.txt");
+                File.WriteAllText(filepath, "Bintang Kecil, di langit yang biru, amat banyak menghias angkasa.");
+                repo.Index.Add(filepath);
+                var commit2 = repo.Commit("Nyanyian anak bangsa", new Author("Legend", "hist@jakarta.id"));
+
+                repo.CurrentBranch.ResetHard(commit.Hash);
+
+                Assert.AreEqual(commit.Hash, repo.CurrentBranch.CurrentCommit.Hash);
+
+            }
+        }
+    }
+}

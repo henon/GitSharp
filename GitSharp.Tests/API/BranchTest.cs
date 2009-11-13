499a7d4946112401cfe9b06510671f15f09067bf
henon <meinrad.recheis@gmail.com>
2009-11-13 19:39:40 +0100

    improved paupaw san's reset hard test
    * checks if files are removed as they should
    * the index is clean after reset
    * ... and untracked files are left untouched

--- GitSharp.Tests/API/BranchTest.cs
+++ GitSharp.Tests/API/BranchTest.cs
@@ -1,4 +1,43 @@
-﻿using System;
+﻿/*
+ * Copyright (C) 2009, Paupaw <paupawsan@gmail.com>
+ * Copyright (C) 2009, Henon <meinrad.recheis@gmail.com>
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Git Development Community nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +47,7 @@ using System.IO;
 namespace GitSharp.Tests.API
 {
     [TestFixture]
-    class BranchTest : ApiTestCase
+    public class BranchTest : ApiTestCase
     {
         [Test]
         public void ResetHard()
@@ -20,21 +59,40 @@ namespace GitSharp.Tests.API
                 repo.Index.Add(filepath);
                 var commit = repo.Commit("You feeling lucky punk!?", new Author("IronHide", "transformers@cybertron.com"));
 
-                filepath = Path.Combine(repo.WorkingDirectory, "Bintang Kecil.txt");
-                File.WriteAllText(filepath, "Bintang Kecil, di langit yang biru, amat banyak menghias angkasa.");
-                repo.Index.Add(filepath);
+                // now changing file from first commit
+                File.AppendAllText(filepath, "... and a new hat too.");
+                // and add new file
+                string filepath1 = Path.Combine(repo.WorkingDirectory, "Bintang Kecil.txt");
+                File.WriteAllText(filepath1, "Bintang Kecil, di langit yang biru, amat banyak menghias angkasa.");
+                repo.Index.Add(filepath, filepath1);
                 var commit2 = repo.Commit("Nyanyian anak bangsa", new Author("Legend", "hist@jakarta.id"));
 
+                // adding an untracked file which should not be removed by reset hard
+                var filepath2 = Path.Combine(repo.WorkingDirectory, "some untracked file");
+                File.WriteAllText(filepath2, "untracked content");
+
+                // git reset --hard
                 repo.CurrentBranch.ResetHard(commit.Hash);
 
                 Assert.AreEqual(commit.Hash, repo.CurrentBranch.CurrentCommit.Hash);
+                Assert.IsFalse(new FileInfo(filepath1).Exists);
+                Assert.AreEqual("hey, paupaw gets new shoes!", File.ReadAllText(filepath));
+                var status = repo.Status;
+                Assert.AreEqual(0, status.Added.Count);
+                Assert.AreEqual(0, status.Modified.Count);
+                Assert.AreEqual(0, status.Missing.Count);
+                Assert.AreEqual(0, status.Removed.Count);
+                Assert.AreEqual(0, status.Staged.Count);
+                Assert.AreEqual(1, status.Untracked.Count);
+                Assert.IsTrue(new FileInfo(filepath2).Exists);
 
-                filepath = Path.Combine(repo.WorkingDirectory, "for me.txt");
-                File.WriteAllText(filepath, "This should be fine if reset hard was working fine.");
-                repo.Index.Add(filepath);
+                var filepath3 = Path.Combine(repo.WorkingDirectory, "for me.txt");
+                File.WriteAllText(filepath3, "This should be fine if reset hard was working fine.");
+                repo.Index.Add(filepath3);
                 var commit3 = repo.Commit("commit after hard reset", new Author("paupaw", "paupaw@home.jp"));
 
                 Assert.AreEqual(commit3.Hash, repo.CurrentBranch.CurrentCommit.Hash);
+                Assert.AreEqual(commit3.Parent, commit);
             }
         }
     }

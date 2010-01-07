cf2239557faf14b09bf953060167637f0dafa4c5
henon <meinrad.recheis@gmail.com>
2010-01-07 23:52:02 +0100

    initialized first version of the online documentation

---
+++ GitSharp/SharedAssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("The Git Development Community")]
+[assembly: AssemblyCopyright("Copyright © 2009 The GitSharp Team")]
+[assembly: ComVisible(false)]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif

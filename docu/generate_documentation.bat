070839dfd5a103a22c7d5e6630fa1310081c74f7
henon <meinrad.recheis@gmail.com>
2010-01-09 09:22:05 +0100

    removing accidentally checked in output folder

--- docu/generate_documentation.bat
+++ docu/generate_documentation.bat
@@ -1,3 +1,3 @@
-REM Note: this assues, that you have built gitsharp using visual studio before switching to gh-pages branch.
-docu ../GitSharp/bin/Debug/GitSharp.dll
+REM Note: this assues, that you have built gitsharp using the build script before switching to gh-pages branch.
+docu ../build/net-3.5-release/bin/GitSharp.dll
 xcopy /S /Y output\*.* ..

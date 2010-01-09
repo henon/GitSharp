1f6f933af2a49c3b7e13c206b9a010013d1fed23
henon <meinrad.recheis@gmail.com>
2010-01-09 10:11:56 +0100

    added more documentation for the classes AbstractObject, Author, Blob, Branch, Change

--- docu/generate_documentation.bat
+++ docu/generate_documentation.bat
@@ -1,3 +1,8 @@
-REM Note: this assues, that you have built gitsharp using the build script before switching to gh-pages branch.
-docu ../build/net-3.5-release/bin/GitSharp.dll
+rem ==========================
+rem Generates the documentation
+rem ==========================
+rem
+rem Note: this assumes, that you have built gitsharp using the build script before switching to gh-pages branch.
+
+docu ../build/net-3.5-debug/bin/GitSharp.dll
 xcopy /S /Y output\*.* ..

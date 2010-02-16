840aef55f151d03af33f4a2e3d3aee4dfed96fb4
henon <meinrad.recheis@gmail.com>
2010-02-17 00:40:21 +0100

    updated documentation to commit 1c16ac21c74769dc32c92107f65b238f9f54f8e7

--- docu/generate_documentation.bat
+++
@@ -1,8 +0,0 @@
-rem ==========================
-rem Generates the documentation
-rem ==========================
-rem
-rem Note: this assumes, that you have built the debug version of gitsharp with visual studio.
-
-docu ../GitSharp/bin/Debug/GitSharp.dll
-xcopy /S /Y output\*.* ..

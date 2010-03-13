0cbb4854946aa4ec05f980da3b0da0eec42e0585
nulltoken <emeric.fermas@gmail.com>
2010-03-13 17:16:23 +0100

    Converted RemoteRefUpdate.cs line endings to CRLF.

--- GitSharp.Core/Transport/RemoteRefUpdate.cs
+++ GitSharp.Core/Transport/RemoteRefUpdate.cs
@@ -43,7 +43,7 @@ namespace GitSharp.Core.Transport
 
     public class RemoteRefUpdate
     {
-		[Serializable]
+        [Serializable]
         public enum UpdateStatus
         {
             NOT_ATTEMPTED,
@@ -70,8 +70,8 @@ namespace GitSharp.Core.Transport
 
         public RemoteRefUpdate(Repository localDb, string srcRef, string remoteName, bool forceUpdate, string localName, ObjectId expectedOldObjectId)
         {
-			if (localDb == null)
-				throw new ArgumentNullException ("localDb");
+            if (localDb == null)
+                throw new ArgumentNullException("localDb");
             if (remoteName == null)
                 throw new ArgumentException("Remote name can't be null.");
 
@@ -99,8 +99,8 @@ namespace GitSharp.Core.Transport
         public RemoteRefUpdate(RemoteRefUpdate baseUpdate, ObjectId newExpectedOldObjectId)
             : this(baseUpdate._localDb, baseUpdate.SourceRef, baseUpdate.RemoteName, baseUpdate.ForceUpdate, (baseUpdate.TrackingRefUpdate == null ? null : baseUpdate.TrackingRefUpdate.LocalName), newExpectedOldObjectId)
         {
-			if (baseUpdate == null)
-				throw new ArgumentNullException ("baseUpdate");
+            if (baseUpdate == null)
+                throw new ArgumentNullException("baseUpdate");
         }
 
         public bool IsExpectingOldObjectId

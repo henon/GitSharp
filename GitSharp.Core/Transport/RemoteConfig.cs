91b5a279dffb7f86dccb230d04d854b172fe9559
nulltoken <emeric.fermas@gmail.com>
2010-03-28 08:51:33 +0200

    Reviewing transport related code against JGit commit 046198cf5f21e5a63e8ec0ecde2ef3fe21db2eae.

--- GitSharp.Core/Transport/RemoteConfig.cs
+++ GitSharp.Core/Transport/RemoteConfig.cs
@@ -102,16 +102,83 @@ namespace GitSharp.Core.Transport
 			return result;
 		}
 
+        /// <summary>
+        /// local name this remote configuration is recognized as
+        /// </summary>
 		public string Name { get; private set; }
+
+        /// <summary>
+        /// all configured URIs under this remote
+        /// </summary>
 		public List<URIish> URIs { get; private set; }
+
+        /// <summary>
+        /// all configured push-only URIs under this remote.
+        /// </summary>
 		public List<URIish> PushURIs { get; private set; }
+
+        /// <summary>
+        /// Remembered specifications for fetching from a repository.
+        /// </summary>
 		public List<RefSpec> Fetch { get; private set; }
+
+        /// <summary>
+        /// Remembered specifications for pushing to a repository.
+        /// </summary>
 		public List<RefSpec> Push { get; private set; }
+
+        /// <summary>
+        /// Override for the location of 'git-upload-pack' on the remote system.
+        /// <para/>
+        /// This value is only useful for an SSH style connection, where Git is
+        /// asking the remote system to execute a program that provides the necessary
+        /// network protocol.
+        /// <para/>
+        /// returns location of 'git-upload-pack' on the remote system. If no
+        /// location has been configured the default of 'git-upload-pack' is
+        /// returned instead.
+        /// </summary>
 		public string UploadPack { get; private set; }
+
+        /// <summary>
+        /// Override for the location of 'git-receive-pack' on the remote system.
+        /// <para/>
+        /// This value is only useful for an SSH style connection, where Git is
+        /// asking the remote system to execute a program that provides the necessary
+        /// network protocol.
+        /// <para/>
+        /// returns location of 'git-receive-pack' on the remote system. If no
+        /// location has been configured the default of 'git-receive-pack' is
+        /// returned instead.
+        /// </summary>
 		public string ReceivePack { get; private set; }
+
+        /// <summary>
+        /// Get the description of how annotated tags should be treated during fetch.
+        /// <para/>
+        /// returns option indicating the behavior of annotated tags in fetch.
+        /// </summary>
 		public TagOpt TagOpt { get; private set; }
+
+        /// <summary>
+        /// mirror flag to automatically delete remote refs.
+        /// <para/>
+        /// true if pushing to the remote automatically deletes remote refs
+        /// </summary>
 		public bool Mirror { get; set; }
 
+        /// <summary>
+        /// Parse a remote block from an existing configuration file.
+        /// <para/>
+        /// This constructor succeeds even if the requested remote is not defined
+        /// within the supplied configuration file. If that occurs then there will be
+        /// no URIs and no ref specifications known to the new instance.
+        /// </summary>
+        /// <param name="rc">
+        /// the existing configuration to get the remote settings from.
+        /// The configuration must already be loaded into memory.
+        /// </param>
+        /// <param name="remoteName">subsection key indicating the name of this remote.</param>
 		public RemoteConfig(Config rc, string remoteName)
 		{
 			if (rc == null)
@@ -159,6 +226,10 @@ namespace GitSharp.Core.Transport
 			Timeout = rc.getInt(Section, Name, KeyTimeout, 0);
 		}
 
+        /// <summary>
+        /// Update this remote's definition within the configuration.
+        /// </summary>
+        /// <param name="rc">the configuration file to store ourselves into.</param>
 		public void Update(Config rc)
 		{
 			if (rc == null)
@@ -210,7 +281,19 @@ namespace GitSharp.Core.Transport
 			}
 		}
 
-		private void Set(Config rc, string key, int currentValue, IEquatable<int> defaultValue)
+        private void Set(Config rc, string key, bool currentValue, bool defaultValue)
+        {
+            if (defaultValue == currentValue)
+            {
+                Unset(rc, key);
+            }
+            else
+            {
+                rc.setBoolean(Section, Name, key, currentValue);
+            }
+        }
+        
+        private void Set(Config rc, string key, int currentValue, IEquatable<int> defaultValue)
 		{
 			if (defaultValue.Equals(currentValue))
 			{
@@ -222,23 +305,16 @@ namespace GitSharp.Core.Transport
 			}
 		}
 
-		private void Set(Config rc, string key, bool currentValue, bool defaultValue)
-		{
-			if (defaultValue == currentValue)
-			{
-				Unset(rc, key);
-			}
-			else
-			{
-				rc.setBoolean(Section, Name, key, currentValue);
-			}
-		}
-
 		private void Unset(Config rc, string key)
 		{
 			rc.unset(Section, Name, key);
 		}
 
+        /// <summary>
+        /// Add a new URI to the end of the list of URIs.
+        /// </summary>
+        /// <param name="toAdd">the new URI to add to this remote.</param>
+        /// <returns>true if the URI was added; false if it already exists.</returns>
 		public bool AddURI(URIish toAdd)
 		{
 			if (URIs.Contains(toAdd)) return false;
@@ -247,23 +323,21 @@ namespace GitSharp.Core.Transport
 			return true;
 		}
 
+        /// <summary>
+        /// Remove a URI from the list of URIs.
+        /// </summary>
+        /// <param name="toRemove">the URI to remove from this remote.</param>
+        /// <returns>true if the URI was added; false if it already exists.</returns>
 		public bool RemoveURI(URIish toRemove)
 		{
 			return URIs.Remove(toRemove);
 		}
 
-		public bool AddFetchRefSpec(RefSpec s)
-		{
-			if (Fetch.Contains(s))
-			{
-				return false;
-			}
-
-			Fetch.Add(s);
-
-			return true;
-		}
-
+        /// <summary>
+        /// Add a new push-only URI to the end of the list of URIs.
+        /// </summary>
+        /// <param name="toAdd">the new URI to add to this remote.</param>
+        /// <returns>true if the URI was added; false if it already exists.</returns>
 		public bool AddPushURI(URIish toAdd)
 		{
 			if (PushURIs.Contains(toAdd)) return false;
@@ -272,28 +346,74 @@ namespace GitSharp.Core.Transport
 			return true;
 		}
 
+        /// <summary>
+        /// Remove a push-only URI from the list of URIs.
+        /// </summary>
+        /// <param name="toRemove">the URI to remove from this remote.</param>
+        /// <returns>true if the URI was added; false if it already exists.</returns>
+        public bool RemovePushURI(URIish toRemove)
+        {
+            return PushURIs.Remove(toRemove);
+        }
+
+        /// <summary>
+        /// Add a new fetch RefSpec to this remote.
+        /// </summary>
+        /// <param name="s">the new specification to add.</param>
+        /// <returns>true if the specification was added; false if it already exists.</returns>
+        public bool AddFetchRefSpec(RefSpec s)
+        {
+            if (Fetch.Contains(s))
+            {
+                return false;
+            }
+
+            Fetch.Add(s);
+
+            return true;
+        }
+
+        /// <summary>
+        /// Override existing fetch specifications with new ones.
+        /// </summary>
+        /// <param name="specs">
+        /// list of fetch specifications to set. List is copied, it can be
+        /// modified after this call.
+        /// </param>
 		public void SetFetchRefSpecs(List<RefSpec> specs)
 		{
 			Fetch.Clear();
 			Fetch.AddRange(specs);
 		}
 
+        /// <summary>
+        /// Override existing push specifications with new ones.
+        /// </summary>
+        /// <param name="specs">
+        /// list of push specifications to set. List is copied, it can be
+        /// modified after this call.
+        /// </param>
 		public void SetPushRefSpecs(List<RefSpec> specs)
 		{
 			Push.Clear();
 			Push.AddRange(specs);
 		}
 
-		public bool RemovePushURI(URIish toRemove)
-		{
-			return PushURIs.Remove(toRemove);
-		}
-
-		public bool RemoveFetchRefSpec(RefSpec s)
+		/// <summary>
+        /// Remove a fetch RefSpec from this remote.
+		/// </summary>
+        /// <param name="s">the specification to remove.</param>
+        /// <returns>true if the specification existed and was removed.</returns>
+        public bool RemoveFetchRefSpec(RefSpec s)
 		{
 			return Fetch.Remove(s);
 		}
 
+        /// <summary>
+        /// Add a new push RefSpec to this remote.
+        /// </summary>
+        /// <param name="s">the new specification to add.</param>
+        /// <returns>true if the specification was added; false if it already exists.</returns>
 		public bool AddPushRefSpec(RefSpec s)
 		{
 			if (Push.Contains(s)) return false;
@@ -302,16 +422,32 @@ namespace GitSharp.Core.Transport
 			return true;
 		}
 
+        /// <summary>
+        /// Remove a push RefSpec from this remote.
+        /// </summary>
+        /// <param name="s">the specification to remove.</param>
+        /// <returns>true if the specification existed and was removed.</returns>
 		public bool RemovePushRefSpec(RefSpec s)
 		{
 			return Push.Remove(s);
 		}
 
+        /// <summary>
+        /// Set the description of how annotated tags should be treated on fetch.
+        /// </summary>
+        /// <param name="option">method to use when handling annotated tags.</param>
 		public void SetTagOpt(TagOpt option)
 		{
 			TagOpt = option ?? TagOpt.AUTO_FOLLOW;
 		}
 
+        /// <summary>
+        /// timeout before willing to abort an IO call.
+        /// <para/>
+        /// number of seconds to wait (with no data transfer occurring)
+        /// before aborting an IO read or write operation with this
+        /// remote.  A timeout of 0 will block indefinitely.
+        /// </summary>
 		public int Timeout { get; set; }
 	}
 }
